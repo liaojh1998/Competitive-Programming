4caa9f8e0ad5a361aa75fb8acd5186c0c6037391
BuildTools <liaojh98@gmail.com>
2017-01-10 17:46:27 -0600

    1/10/2017

---
+++ UVa/Competitive Programming 3/Problem Solving Paradigms/Complete Search/Iterative (Three or More Nested Loops, Harder)/11565.cpp
@@ -0,0 +1,135 @@
+//Title: Simple Equations
+//Type: Complete Search
+//Complexity: O(4C) - O(2*sqrt(C)*2*sqrt(C))
+//Solution: The 3rd equation, x^2 + y^2 + z^2 = C, generates the lower bound and upper bound of the problem
+//Notice 3 1 3, 11 48 41 should get No solution. because all 3 integers must be different.
+//Anything else may be pruned by using algebra
+#include <bits/stdc++.h>
+//#define getchar() (getchar_unlocked()) //For hackerrank
+using namespace std;
+//inline if read speed is needed, huge compilation time required though
+int readInt(int &n){
+	register int c;
+	n = 0;
+	bool neg = false;
+	c = getchar();
+	if(!(~c)) return -1;
+	for(; ((c < 48 || c > 57)) && c != '-' && ~c; c = getchar());
+	if(c == '-') neg = true, c = getchar();
+	for(; c > 47 && c < 58 && ~c; c = getchar()){n = (n<<1)+(n<<3)+c-48;}
+	if(neg) n = -n;
+	return 1;
+}
+int readLL(long long &n){
+	register int c;
+	n = 0;
+	bool neg = false;
+	c = getchar();
+	if(!(~c)) return -1;
+	for(; ((c < 48 || c > 57)) && c != '-' && ~c; c = getchar());
+	if(c == '-') neg = true, c = getchar();
+	for(; c > 47 && c < 58 && ~c; c = getchar()){n = (n<<1)+(n<<3)+c-48;}
+	if(neg) n = -n;
+	return 1;
+}
+int readDouble(double &n){
+	register int c;
+	n = 0;
+	bool neg = false;
+	c = getchar();
+	if(!(~c)) return -1;
+	for(; ((c < 48 || c > 57)) && c != '-' && c != '.' && ~c; c = getchar());
+	if(c == '-') neg = true, c = getchar();
+	if(c != '.'){
+		for(; c > 47 && c < 58 && ~c; c = getchar()){n = n*10+c-48;}
+		if(c != '.'){
+			if(neg) n = -n;
+			return 1;
+		}
+	}
+	double div = 1;
+	for(c = getchar(); c > 47 && c < 58 && ~c; c = getchar()){div *= 10, n = n*10+c-48;}
+	n /= div;
+	if(neg) n = -n;
+	return 1;
+}
+int readStr(char *str){
+	register char c = getchar();
+	register int len = 0;
+	if(!(~c)) return -1;
+	while(c < 33 && ~c) c = getchar(); //32 if space should be included
+	//32 if stop at space, 10 if stop at next line
+	while(c != 10 && c != 32 && ~c){
+		str[len++] = c;
+		c = getchar();
+	}
+	str[len] = '\0';
+	//Just in case if needs to return arriving at end of line
+	//if(c == 10) return 10;
+	return 1;
+}
+int readStr(char *str, int len){
+	//Reading string of size len
+	for(int i = 0; i < len; i++){
+		str[i] = getchar();
+		if(!(~str[i])){
+			str[i] = '\0';
+			return -1;
+		}
+		//Remove this if str can't go to next line
+		/*if(str[i] == 10){
+			str[i] = '\0';
+			return 10;
+		}*/
+	}
+	str[len] = '\0';
+	return 1;
+}
+void readIntArr(int *n, int len){
+	for(int i = 0; i < len; i++)
+		if(!(~readInt(n[i]))){
+			n[i] = 0;
+			return;
+		}
+}
+void readLLArr(long long *n, int len){
+	for(int i = 0; i < len; i++)
+		if(!(~readLL(n[i]))){
+			n[i] = 0;
+			return;
+		}
+}
+void readDoubleArr(double *n, int len){
+	for(int i = 0; i < len; i++)
+		if(!(~readDouble(n[i]))){
+			n[i] = 0;
+			return;
+		}
+}
+
+int ab(int n){
+	return n < 0 ? -n : n;
+}
+int main(){
+	//freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "w", stdout);
+	int N, A, B, C;
+	readInt(N);
+	while(N--){
+		readInt(A);
+		readInt(B);
+		readInt(C);
+		bool completed = false;
+		for(int x = -(int)sqrt(C); x*x <= C && !completed; x++)
+			for(int y = -(int)sqrt(C-(x*x)); (x*x+y*y) <= C && !completed; y++){
+				if(!x || !y || x == y || y == (A-x-y) || x == (A-x-y)) continue;
+				if((A*A-C) == (2*x*y + 2*y*(A-x-y) + 2*x*(A-x-y)) && x*y*(A-x-y) == B){
+					printf("%d %d %d\n", x, y, (A-x-y));
+					completed = true;
+				}
+			}
+		if(!completed)
+			printf("No solution.\n");
+	}
+	return 0;
+}
