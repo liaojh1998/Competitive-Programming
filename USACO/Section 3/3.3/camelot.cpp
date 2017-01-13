61f9071958a6a687a915e9e87a93f670944b7274
BuildTools <liaojh98@gmail.com>
2017-01-13 17:15:27 -0600

    1/13/2017
    Back to USACO training for fun :3

--- USACO/Section 3/3.3/camelot.cpp
+++ USACO/Section 3/3.3/camelot.cpp
@@ -5,71 +5,190 @@ LANG: C++
 */
 //Title: camelot
 //Type: BFS
-//Solution: Trying so hard
-#include <fstream>
-#include <cstring>
-#include <queue>
+//Solution: First bfs the knight and the minimum distance to reach a spot, second bfs the knight with the king and find the minimum distance for the king to reach a spot
+#include <bits/stdc++.h>
+//#define getchar() (getchar_unlocked()) //For hackerrank
 using namespace std;
-ifstream fin("camelot.in");
-ofstream fout("camelot.out");
-char temp;
-int R, C, Kc, Kr, kcount, dist[4][31][27], kr[4], kc[4], akr, akc;
-int kdirc[8] = {-2, -1, 1, 2, 2, 1, -1, -2};
-int kdirr[8] = {-1, -2, -2, -1, 1, 2, 2, 1};
-struct p{
-	int k, r, c, p;
-}pt, pt2;
-queue<p> q;
+//inline if read speed is needed, huge compilation time required though
+int readInt(int &n){
+	register int c;
+	n = 0;
+	bool neg = false;
+	c = getchar();
+	if(!(~c)) return -1;
+	for(; ((c < 48 || c > 57)) && c != '-' && ~c; c = getchar());
+	if(c == '-') neg = true, c = getchar();
+	for(; c > 47 && c < 58 && ~c; c = getchar()){n = (n<<1)+(n<<3)+c-48;}
+	if(neg) n = -n;
+	return 1;
+}
+int readLL(long long &n){
+	register int c;
+	n = 0;
+	bool neg = false;
+	c = getchar();
+	if(!(~c)) return -1;
+	for(; ((c < 48 || c > 57)) && c != '-' && ~c; c = getchar());
+	if(c == '-') neg = true, c = getchar();
+	for(; c > 47 && c < 58 && ~c; c = getchar()){n = (n<<1)+(n<<3)+c-48;}
+	if(neg) n = -n;
+	return 1;
+}
+int readDouble(double &n){
+	register int c;
+	n = 0;
+	bool neg = false;
+	c = getchar();
+	if(!(~c)) return -1;
+	for(; ((c < 48 || c > 57)) && c != '-' && c != '.' && ~c; c = getchar());
+	if(c == '-') neg = true, c = getchar();
+	if(c != '.'){
+		for(; c > 47 && c < 58 && ~c; c = getchar()){n = n*10+c-48;}
+		if(c != '.'){
+			if(neg) n = -n;
+			return 1;
+		}
+	}
+	double div = 1;
+	for(c = getchar(); c > 47 && c < 58 && ~c; c = getchar()){div *= 10, n = n*10+c-48;}
+	n /= div;
+	if(neg) n = -n;
+	return 1;
+}
+int readStr(char *str){
+	register char c = getchar();
+	register int len = 0;
+	if(!(~c)) return -1;
+	while(c < 33 && ~c) c = getchar(); //32 if space should be included
+	//32 if stop at space, 10 if stop at next line
+	while(c != 10 && c != 32 && ~c){
+		str[len++] = c;
+		c = getchar();
+	}
+	str[len] = '\0';
+	//Just in case if needs to return arriving at end of line
+	//if(c == 10) return 10;
+	return 1;
+}
+int readStr(char *str, int len){
+	//Reading string of size len
+	for(int i = 0; i < len; i++){
+		str[i] = getchar();
+		if(!(~str[i])){
+			str[i] = '\0';
+			return -1;
+		}
+		//Remove this if str can't go to next line
+		/*if(str[i] == 10){
+			str[i] = '\0';
+			return 10;
+		}*/
+	}
+	str[len] = '\0';
+	return 1;
+}
+void readIntArr(int *n, int len){
+	for(int i = 0; i < len; i++)
+		if(!(~readInt(n[i]))){
+			n[i] = 0;
+			return;
+		}
+}
+void readLLArr(long long *n, int len){
+	for(int i = 0; i < len; i++)
+		if(!(~readLL(n[i]))){
+			n[i] = 0;
+			return;
+		}
+}
+void readDoubleArr(double *n, int len){
+	for(int i = 0; i < len; i++)
+		if(!(~readDouble(n[i]))){
+			n[i] = 0;
+			return;
+		}
+}
+
+#define pa(a, b) (pair<int, int>(a, b))
+int ab(int a){
+	return (a < 0) ? -a: a;
+}
 int main(){
-	//Instantiation
-	fin >> R >> C;
-	fin >> temp >> Kr;
-	Kc = temp - 64;
-	while(!fin.eof()){
-		fin >> temp >> kr[kcount];
-		kc[kcount] = temp - 64;
-		kcount++;
-	}
-	for(int i = 0; i < kcount; i++){
-		for(int j = 0; j <= R; j++){
-			for(int k = 0; k <= C; k++){
-				dist[i][j][k] = -1;
+	freopen("camelot.in", "r", stdin);
+	freopen("camelot.out", "w", stdout);
+	int movex[8] = {1, 2, 2, 1, -1, -2, -2, -1};
+	int movey[8] = {-2, -1, 1, 2, 2, 1, -1, -2};
+	int R, C, kx, ky, kodist[35][35], kdist[35][35], kcost[35][35], dist[35][35], cost[35][35];
+	bool vis[35][35];
+	readInt(R);
+	readInt(C);
+	kx = getchar() - 'A';
+	readInt(ky);
+	ky--;
+	for(int i = 0; i < R; i++)
+		for(int j = 0; j < C; j++)
+			kodist[i][j] = ab(j - kx) > ab(i - ky) ? ab(j - kx) : ab(i - ky);
+	char c = getchar();
+	bool hasKnight = false;
+	memset(cost, 0, sizeof(cost));
+	memset(vis, 0, sizeof(vis));
+	memset(kcost, 1, sizeof(kcost));
+	while(~c){
+		hasKnight = true;
+		int y, x = c - 'A';
+		readInt(y);
+		y--;
+		//First bfs, knight only
+		memset(dist, 1, sizeof(dist));
+		queue<pair<int, int> > q;
+		dist[y][x] = 0;
+		q.push(pa(y, x)); //R, C : y, x
+		while(!q.empty()){
+			pair<int, int> temp = q.front(); q.pop();
+			cost[temp.first][temp.second] += dist[temp.first][temp.second];
+			vis[temp.first][temp.second] = true;
+			for(int i = 0; i < 8; i++)
+				if(temp.first+movey[i] >= 0 && temp.first+movey[i] < R && temp.second+movex[i] >= 0 && temp.second+movex[i] < C
+					&& dist[temp.first+movey[i]][temp.second+movex[i]] > dist[temp.first][temp.second] + 1){
+					dist[temp.first+movey[i]][temp.second+movex[i]] = dist[temp.first][temp.second] + 1;
+					q.push(pa(temp.first+movey[i], temp.second+movex[i]));
+				}
+		}
+		//Second bfs, king with knight
+		//Fill first with original distance
+		for(int i = 0; i < R; i++)
+			for(int j = 0; j < C; j++)
+				kdist[i][j] = kodist[i][j];
+		//bfs
+		for(int i = 0; i < R; i++)
+			for(int j = 0; j < C; j++){
+				q.push(pa(i, j));
+				while(!q.empty()){
+					pair<int, int> temp = q.front(); q.pop();
+					for(int k = 0; k < 8; k++)
+						if(temp.first+movey[k] >= 0 && temp.first+movey[k] < R && temp.second+movex[k] >= 0 && temp.second+movex[k] < C
+							&& kdist[temp.first+movey[k]][temp.second+movex[k]] + dist[temp.first+movey[k]][temp.second+movex[k]] > kdist[temp.first][temp.second] + dist[temp.first][temp.second] + 1){
+							//Subtraction to substitute the new knight to carry the king
+							kdist[temp.first+movey[k]][temp.second+movex[k]] = kdist[temp.first][temp.second] + dist[temp.first][temp.second] + 1 - dist[temp.first+movey[k]][temp.second+movex[k]];
+							q.push(pa(temp.first+movey[k], temp.second+movex[k]));
+						}
+				}
 			}
-		}
+		for(int i = 0; i < R; i++)
+			for(int j = 0; j < C; j++)
+				if(kdist[i][j] < kcost[i][j])
+					kcost[i][j] = kdist[i][j];
+		c = getchar();
 	}
-	for(int i = 0; i < kcount; i++){
-		dist[i][kr[i]][kc[i]] = 0;
-		pt.k = i;
-		pt.r = kr[i];
-		pt.c = kc[i];
-		q.push(pt);
-	}
-	//BFS for knights only
-	while(!q.empty()){
-		pt = q.front();
-		q.pop();
-		bool br = 1;
-		for(int i = 0; i < kcount; i++){
-			if(dist[i][pt.r][pt.c] == -1){
-				br = 0;
-				break;
-			}
-		}
-		if(br){
-			akr = pt.r;
-			akc = pt.c;
-			break;
-		}
-		for(int i = 0; i < 8; i++){
-			pt2.k = pt.k;
-			pt2.r = pt.r+kdirr[i];
-			pt2.c = pt.c+kdirc[i];
-			if(pt.r+kdirr[i] >= 1 && pt.r+kdirr[i] <= R && pt.c+kdirc[i] >= 1 && pt.c+kdirc[i] <= C
-				&& dist[pt.k][pt.r+kdirr[i]][pt.c+kdirc[i]] == -1){
-				dist[pt.k][pt.r+kdirr[i]][pt.c+kdirc[i]] = dist[pt.k][pt.r][pt.c]+1;
-				q.push(pt2);
-			}
-		}
+	if(!hasKnight)
+		printf("0\n");
+	else{
+		int minSum = 1e9;
+		for(int i = 0; i < R; i++)
+			for(int j = 0; j < C; j++)
+				if(vis[i][j] && cost[i][j] + kcost[i][j] < minSum)
+					minSum = cost[i][j] + kcost[i][j];
+		printf("%d\n", minSum);
 	}
 	return 0;
 }
