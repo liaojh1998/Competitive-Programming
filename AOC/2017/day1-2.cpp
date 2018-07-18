e4e4b3899398c9b2357fc66f2782265ccbf068fc
liaojh1998 <liaojh1998@gmail.com>
2018-07-18 00:10:53 -0500

    Starting out Advent of Code for fun :)

---
+++ AOC/2017/day1-2.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	string str;
+	getline(cin, str);
+	int size = str.size();
+	long long sum = 0;
+	for(int i = 0; i < size; i++)
+		if(str[i] == str[(i+size/2)%size])
+			sum += str[i] - '0';
+	cout << sum << endl;
+	return 0;
+}
+
